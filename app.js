549aa681b06669e205c805d3504941c273020cdb
Darrell Chaddock <webtastic.dc@gmail.com>
2022-12-29 21:02:41 -0800

    Simple modal project finished

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 // Create variables for Open Button and Modal Container and the Close Button
+let openBtn = document.getElementById('open-btn');
+let modalContainer = document.getElementById('modal-container');
+let closeBtn = document.getElementById('close-btn');
+
+// create Event Listeners
+openBtn.addEventListener('click', function() {
+
+    modalContainer.style.display = 'block';
+});
+
+closeBtn.addEventListener('click', function(){
+
+    modalContainer.style.display = 'none';
+})
+
+// e.target returns the element that triggers the event (the Window Object in the below example)
+// when the modal appears we want to be able to click outside of it to make it close
+window.addEventListener('click', function(e){
+
+    if(e.target === modalContainer) {
+        modalContainer.style.display = 'none';
+    }
+});
